845b4146868ec8ad48973b2909d16bc1da300909
huangyuanhao <307975180@qq.com>
2019-03-06 20:31:53 +0800

    build encyclopedia view

--- app/src/main/java/com/example/oneroad/fragments/NavEncyclopediaFragment.java
+++ app/src/main/java/com/example/oneroad/fragments/NavEncyclopediaFragment.java
@@ -1,26 +1,32 @@
 package com.example.oneroad.fragments;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.oneroad.R;
+import com.example.oneroad.classes.PrimePageListGoods;
+import com.example.oneroad.utils.GlideImageLoader;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.Transformer;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.BitmapCallback;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link NavEncyclopediaFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link NavEncyclopediaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+
+
 public class NavEncyclopediaFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -30,19 +36,14 @@ public class NavEncyclopediaFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
+    private List<String> images = new ArrayList<>();
+    private List<String> titles = new ArrayList<>();
+
     public NavEncyclopediaFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NavEncyclopediaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static NavEncyclopediaFragment newInstance(String param1, String param2) {
         NavEncyclopediaFragment fragment = new NavEncyclopediaFragment();
         Bundle args = new Bundle();
@@ -65,10 +66,13 @@ public class NavEncyclopediaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nav_encyclopedia, container, false);
+        View view =  inflater.inflate(R.layout.fragment_nav_encyclopedia, container, false);
+        setBanner(view);
+
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
+
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -81,18 +85,68 @@ public class NavEncyclopediaFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+    private void setBanner(View view){
+        //配置 images 和 titles
+        for (int i = 0 ; i < 4 ; i++){
+            images.add("http://47.107.132.227/form");
+            titles.add("这里是测试用例");
+        }
+
+        Banner banner = (Banner) view.findViewById(R.id.nav_encyclopedia_banner);
+        banner.setImages(images).setImageLoader(new GlideImageLoader());
+
+        //设置banner样式
+        banner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR_TITLE);
+        //设置图片加载器
+        banner.setImageLoader(new GlideImageLoader());
+        //设置图片集合
+        banner.setImages(images);
+        //设置banner动画效果
+        banner.setBannerAnimation(Transformer.DepthPage);
+        //设置标题集合（当banner样式有显示title时）
+        banner.setBannerTitles(titles);
+        //设置自动轮播，默认为true
+        banner.isAutoPlay(true);
+        //设置轮播时间
+        banner.setDelayTime(1500);
+        //设置指示器位置（当banner模式中有指示器时）
+        banner.setIndicatorGravity(BannerConfig.CENTER);
+        //banner设置方法全部调用完毕时最后调用
+        banner.start();
+    }
+
+    public void getImage()
+    {
+        String url = "http://47.107.132.227/form";
+        OkHttpUtils
+                .get()//
+                .url(url)//
+                .tag(this)//
+                .build()//
+                .connTimeOut(20000)//连接超时
+                .readTimeOut(20000)//读取超时
+                .writeTimeOut(20000)//写超时
+                .execute(new BitmapCallback()
+                {
+                    @Override
+                    public void onError(Call call, Exception e, int id)
+                    {
+                        Log.d("tag","onError:" + e.getMessage());
+                    }
+
+                    @Override
+                    public void onResponse(Bitmap bitmap, int id)
+                    {
+                        Log.e("TAG", "onResponse：complete");
+//                        mainVerticalList.add(new PrimePageListGoods(bitmap));
+                    }
+                });
+    }
+
 }
