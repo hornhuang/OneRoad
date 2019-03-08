11ef805382b0f9751a7a5d91021938dca23d5385
huangyuanhao <307975180@qq.com>
2019-03-08 19:04:46 +0800

    nav rout fragment view build.

--- app/src/main/java/com/example/oneroad/fragments/NavRouteFragment.java
+++ app/src/main/java/com/example/oneroad/fragments/NavRouteFragment.java
@@ -10,14 +10,6 @@ import android.view.ViewGroup;
 
 import com.example.oneroad.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link NavRouteFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link NavRouteFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class NavRouteFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,19 +22,12 @@ public class NavRouteFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
+    View view;
+
     public NavRouteFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NavRouteFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static NavRouteFragment newInstance(String param1, String param2) {
         NavRouteFragment fragment = new NavRouteFragment();
         Bundle args = new Bundle();
@@ -65,7 +50,9 @@ public class NavRouteFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nav_route, container, false);
+        view = inflater.inflate(R.layout.fragment_nav_route, container, false);
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -81,16 +68,6 @@ public class NavRouteFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
